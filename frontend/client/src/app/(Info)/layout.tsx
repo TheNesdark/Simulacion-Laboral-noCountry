54046e44685bdd559a950bf2f7a8670a9c872123
TheNesdark <168293938+TheNesdark@users.noreply.github.com>
2025-10-23 13:28:48 +0000

    implemetacion de los cambios de tarquibrian del otro repositorio en este repositorio

---
+++ frontend/client/src/app/(Info)/layout.tsx
@@ -0,0 +1,32 @@
+import "@/styles/globals.css";
+import { Roboto } from "next/font/google";
+import Header from "@/components/layout/Header";
+import NavBar from "@/components/layout/NavBar";
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  preload: true,
+  fallback: ["system-ui", "arial"],
+});
+
+export const metadata = {
+  title: "CoordiSalud - Info",
+  description: "Sistema de gestión médica y coordinación de salud",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body className={roboto.className}>
+          <Header />
+          <main>{children}</main>
+      </body>
+    </html>
+  );
+}
--- frontend/client/src/app/(Pacientes)/(Home)/components/CategoryCards.tsx
+++ frontend/client/src/app/(Pacientes)/(Home)/components/CategoryCards.tsx
@@ -1,20 +1,25 @@
 "use client";
 
+import Link from 'next/link';
+
 const categories = [
   {
     icon: "medicos-icon",
     alt: "Medicos",
     label: "Medicos",
+    path: "/Medicos"
   },
   {
     icon: "hospital-icon",
     alt: "Hospital",
     label: "Clinicas y Hospitales",
+    path: "/Clinicas"
   },
   {
     icon: "especialidades-icon",
     alt: "Especialidades",
     label: "Especialidades",
+    path: "/Especialidades"
   },
 ];
 
@@ -22,14 +27,16 @@ export default function CategoryCards() {
   return (
     <section className="category-cards">
       {categories.map((category, index) => (
-        <article key={index}>
-          <svg width={50} height={50}>
-            <use
-              href={`/assets/icons/homeIcons-sprites.svg#${category.icon}`}
-            />
-          </svg>
-          {category.label}
-        </article>
+        <Link href={category.path} key={index} className="category-card">
+          <article>
+            <svg width={50} height={50}>
+              <use
+                href={`/assets/icons/homeIcons-sprites.svg#${category.icon}`}
+              />
+            </svg>
+            {category.label}
+          </article>
+        </Link>
       ))}
     </section>
   );
