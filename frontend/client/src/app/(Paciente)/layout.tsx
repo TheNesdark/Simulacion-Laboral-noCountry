de9a55224e7aec1898b14c09da6ea4a1f0133bed
TheNesdark <168293938+TheNesdark@users.noreply.github.com>
2025-10-27 20:49:33 +0000

    refactor(layout): eliminar AuthGuard del layout de paciente
    - Se ha eliminado el componente AuthGuard del layout principal de la sección de pacientes.
    - Se asume que la autenticación se gestionará en niveles inferiores o de forma más específica.

--- frontend/client/src/app/(Paciente)/layout.tsx
+++ frontend/client/src/app/(Paciente)/layout.tsx
@@ -3,7 +3,6 @@ import { Roboto } from "next/font/google";
 import Providers from "@/components/providers";
 import Header from "@/components/layout/Header";
 import NavBar from "@/components/layout/NavBar";
-import AuthGuard from "@/components/AuthGuard"; 
 const roboto = Roboto({
   weight: ["400", "700"],
   subsets: ["latin"],
@@ -26,11 +25,9 @@ export default function RootLayout({
     <html lang="es">
       <body className={roboto.className}>
         <Providers>
-          <AuthGuard>
             <Header />
             <main>{children}</main>
             <NavBar />
-          </AuthGuard>
         </Providers>
       </body>
     </html>
